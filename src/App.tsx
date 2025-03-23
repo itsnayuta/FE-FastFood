737c7f3d1a1298487e8e77b33ff3955cf36aed98
Pham2uanghuy <2uanghuy2k3@gmail.com>
2025-03-23 20:25:22 +0700

    set up

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from "react";
+import {View, Text, StyleSheet} from 'react-native'
+
+
+
+const App: React.FC = () => {
+    return (
+        <View>
+            <Text>Hehe</Text>
+        </View>
+    )
+}
+
+export default App
